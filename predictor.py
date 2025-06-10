e8fdc308ad838369915c51a7f89aecf250aa836b
Davide <davide.baggio.01@gmail.com>
2025-06-10 17:14:08 +0200

    predictor

--- predictor.py
+++ predictor.py
@@ -1,22 +1,37 @@
 import os
 import argparse
 from Bio.PDB import PDBList 
+import pandas as pd
+import lightgbm as lgb
+import xgboost as xgb
 
 def parse_args():
     parser = argparse.ArgumentParser(description="Predict contact types of a given protein structure.")
     parser.add_argument('--input', type=str, required=True, help='PDB ID of the protein structure to predict.')
-    parser.add_argument('--model', type=str, required=True, help='Name of the trained model file.')
+    parser.add_argument('--model', type=str, required=True, help='Type of model to use for prediction (e.g., "lgb", "xgb").')
+    parser.add_argument('--path', type=str, required=True, help='Path to the file containing the model files.')
     return parser.parse_args()
 
-def retrieve_structure(pdb_id):
+def retrieve_features(pdb_id):
     pdb_file = PDBList.retrieve_pdb_file(pdb_id, file_format='pdb', pdir='./data/pdb files/', overwrite=True)
     if not pdb_file:
         raise ValueError(f"Failed to retrieve PDB file for ID: {pdb_id}")
     os.system(f"python3 ./data/script/calc_features.py ./data/pdb files/{pdb_id}.cif -out_dir ./data/output features/")
-    
+    print(f"Features for {pdb_id} retrieved successfully.")
+    return pd.read_csv(f"./data/output features/{pdb_id}_features.csv", sep='\t')
+
+def predict_contacts(pdb_id, model, model_path):
+    features = retrieve_features(pdb_id)
+    if model == 'lgb':
+        loaded_model = lgb.Booster(model_file=model_path)
+        predictions = loaded_model.predict(features)
+    elif model == 'xgb':
+        loaded_model = xgb.Booster(model_file=model_path)
+        predictions = loaded_model.predict(features)
 
 def main():
     args = parse_args()
     pdb_id = args.input
-    model_name = args.model
+    model = args.model
+    model_path = args.path
 
